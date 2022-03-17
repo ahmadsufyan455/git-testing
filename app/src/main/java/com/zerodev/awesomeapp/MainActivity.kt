439f0869ff5b96a7f2e8ab1cdaa4d7b710eb5e6e
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-03-17 13:01:58 +0700

    add comment again

--- app/src/main/java/com/zerodev/awesomeapp/MainActivity.kt
+++ app/src/main/java/com/zerodev/awesomeapp/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // add magic
+        // add magic here
         val firstNumber = binding.num1.text
         val secondNumber = binding.num2.text
 
