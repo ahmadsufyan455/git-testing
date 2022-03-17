6bd4b833eeee242854f73110cfd1fc7c58016341
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-03-17 12:53:48 +0700

    initial commit

---
+++ app/src/main/java/com/zerodev/awesomeapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.zerodev.awesomeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zerodev.awesomeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val firstNumber = binding.num1.text
+        val secondNumber = binding.num2.text
+
+        binding.btnCalculate.setOnClickListener {
+            val result = firstNumber.toString().toInt() + secondNumber.toString().toInt()
+            binding.tvResult.text = result.toString()
+        }
+    }
+}
